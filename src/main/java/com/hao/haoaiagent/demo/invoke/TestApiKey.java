36a34821d659546ae784beee80dc0de8e3e936e8
PyHao <464441474@qq.com>
2026-07-09 18:51:09 +0800

    首次上传

--- src/main/java/com/hao/haoaiagent/demo/invoke/TestApiKey.java
+++
@@ -1,7 +0,0 @@
-package com.hao.haoaiagent.demo.invoke;
-
-public interface TestApiKey {
-    String API_KEY = "sk-ws-H.RYEEDIR.lNZt.MEQCIBA88E0KcrQPUzWfcbUEBK3C_-mKGf0dEuexLBxtosGHAiBY0hxyOddDxHnrbM_1I6GTExGtRtopq4xkjEr3RwTG0A";
-//    String model = "qwen3.7-max";
-    String model = "qwen-max";
-}
